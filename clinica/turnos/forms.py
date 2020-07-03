f12a1ba74c2d6e87f6755a6a3cfb779eba5e0946
Juan Martin Almada <juanalmada1395@gmail.com>
2020-07-03 17:57:05 -0300

    Manejo de formularios

--- clinica/turnos/forms.py
+++ clinica/turnos/forms.py
@@ -3,14 +3,20 @@ from django import forms
 
 class TurnosForm(forms.Form):
 
-    nombre = forms.CharField(label='Nombre',required=True)
-    apellido = forms.CharField(label='Apellido',required=True)
-    obra_social = forms.CharField(label='Obra Social',required=True)
-    email = forms.EmailField(label='Email',required=True)
-    edad = forms.IntegerField(label='Edad',required=True)
-    dni = forms.IntegerField(label='DNI',required=True)
-    telefono = forms.CharField(label='Telefono',required=True)
-    direccion = forms.CharField(label='Direccion',required=True)
+    nombre = forms.CharField(label='Nombre y apellido',required=True, widget=forms.TextInput(
+        attrs={'class':"form-control", 'placeholder': "Nombre"}))
+    obra_social = forms.CharField(label='Obra Social',required=True, widget=forms.TextInput(
+        attrs={'class':"form-control", 'placeholder': "Obra Social"}))
+    email = forms.EmailField(label='Email',required=True, widget=forms.EmailInput(
+        attrs={'class':"form-control", 'placeholder': "Email"}))
+    edad = forms.IntegerField(label='Edad',required=True, widget=forms.NumberInput(
+        attrs={'class':"form-control", 'placeholder': "Edad"}))
+    dni = forms.IntegerField(label='DNI',required=True, widget=forms.NumberInput(
+        attrs={'class':"form-control", 'placeholder': "DNI"}))
+    telefono = forms.CharField(label='Telefono',required=True, widget=forms.TextInput(
+        attrs={'class':"form-control", 'placeholder': "Telefono"}))
+    direccion = forms.CharField(label='Direccion',required=True, widget=forms.TextInput(
+        attrs={'class':"form-control", 'placeholder': "Direccion"}))
 
 
 
--- clinica/turnos/views.py
+++ clinica/turnos/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import TurnosForm
+from django.urls import reverse
 
 # Create your views here.
 def turnos(request):
     turno = TurnosForm()
+    if request.method == 'POST':
+        turno = TurnosForm(data=request.POST)
+        if turno.is_valid():
+            nombre = request.POST.get('nombre', '')
+            obra_social = request.POST.get('obra_social', '')
+            email = request.POST.get('email', '')
+            edad = request.POST.get('edad', '')
+            dni = request.POST.get('dni', '')
+            telefono = request.POST.get('telefono', '')
+            direccion = request.POST.get('direccion', '')
+            return redirect(reverse('turnos')+"?OK")
+
     return render(request, 'turnos/turnos.html', {'turno':turno})
     
