414c06ef106e9a7dbaf77bf2cb5e980d27960305
Juan Martin Almada <juanalmada1395@gmail.com>
2020-07-16 22:14:33 -0300

    entrega final-curso
    se envia hasta aca lo hecho para el curso

--- clinica/turnos/admin.py
+++ clinica/turnos/admin.py
@@ -3,8 +3,8 @@ from turnos.models import Turno
 
 
 class TurnoAdmin(admin.ModelAdmin):
-    fields = ['nombre', 'obra_social', 'email', 'edad', 'dni', 'telefono', 'direccion']
-    list_display = ['nombre', 'obra_social', 'email', 'edad','telefono','direccion','fecha_create','fecha_update']
+    fields = ['nombre', 'obra_social', 'email', 'edad', 'dni', 'telefono', 'direccion','fecha_consulta']
+    list_display = ['nombre', 'obra_social', 'fecha_consulta','email', 'edad','telefono','direccion','fecha_create','fecha_update']
     ordering = ['-nombre']
     list_filter = ('nombre', 'obra_social')
 
